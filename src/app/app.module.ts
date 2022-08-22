e5928c9bab3df71bb673d31917c4dd70818eb7dd
Minh. Nguyen Quang - CMC Global DU3.11 <nqminh3@cmcglobal.vn>
2022-08-22 16:19:01 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    MainContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
